8fb46d1cce4bc0ae0ca90391df9f37b8d8e0e60b
rayi ahmad <rayiahmad@student.gunadarma.ac.id>
2026-06-15 15:05:00 +0700

    Refactor TermsConditions component to functional page

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -1,18 +1,7 @@
-import Navbar from "../../components/layouts/Navbar";
-import Footer from "../../components/layouts/Footer";
+import React from "react";
 
-export default function TermsConditions() {
-  return (
-    <>
-      <Navbar session={null} />
+const page = () => {
+  return <div>page</div>;
+};
 
-      <main className="min-h-screen bg-white flex items-center justify-center">
-        <h1 className="text-xl font-semibold text-gray-500">
-          Terms & Conditions Page
-        </h1>
-      </main>
-
-      <Footer />
-    </>
-  );
-}
+export default page;
