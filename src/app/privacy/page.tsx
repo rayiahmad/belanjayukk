02dd874cac406130b2c4cee3ba317b40f1b9eaaa
rayi ahmad <rayiahmad@student.gunadarma.ac.id>
2026-06-15 14:55:59 +0700

    Rename TermsConditions to Privacy and update heading

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,15 +1,13 @@
 import Navbar from "../../components/layouts/Navbar";
 import Footer from "../../components/layouts/Footer";
 
-export default function TermsConditions() {
+export default function Privacy() {
   return (
     <>
       <Navbar session={null} />
 
       <main className="min-h-screen bg-white flex items-center justify-center">
-        <h1 className="text-xl font-semibold text-gray-500">
-          Terms & Conditions Page
-        </h1>
+        <h1 className="text-xl font-semibold text-gray-500">Privacy Page</h1>
       </main>
 
       <Footer />
